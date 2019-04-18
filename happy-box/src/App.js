aa316e9e26ae27824d427e60f3cf3e0ad200cb3f
alejandrok93 <alejandrok93@gmail.com>
2019-04-17 17:34:49 -0700

    Added HTML structure

--- happy-box/src/App.js
+++ happy-box/src/App.js
@@ -3,26 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="container">
+				<div className="background-image">
+					<div className="hero-image-container">
+						<img className="logo" src="./logo" alt="Happy Box logo" />
+						<div className="main-content">
+							<div className="left">
+								<div className="hero-text">
+									<h1>Unbox Happiness!</h1>
+									<p>
+										Sorprende a esa persona espceial con un regalo fuera de lo
+										común
+									</p>
+									<div className="cta-button">Descubre opciones ahora!</div>
+								</div>
+							</div>
+							<div className="right">
+								<img
+									className="hero-image"
+									src="./img/cajas.png"
+									alt="Example boxes"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
