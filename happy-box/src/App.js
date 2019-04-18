b8202615a8aa411c015312298ea022962ba1827d
alejandrok93 <alejandrok93@gmail.com>
2019-04-17 18:32:21 -0700

    Removed unnecessary background-image div

--- happy-box/src/App.js
+++ happy-box/src/App.js
@@ -9,30 +9,28 @@ class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				<div className="background-image">
-					<div className="hero-image-container">
-						<img className="logo" src="./logo" alt="Happy Box logo" />
-						<div className="main-content">
-							<div className="left">
-								<div className="hero-text">
-									<h1>Unbox Happiness!</h1>
-									<p>
-										Sorprende a esa persona espceial con un regalo fuera de lo
-										común
-									</p>
-									<div className="cta-button">Descubre opciones ahora!</div>
-								</div>
+				<div className="hero-image-container">
+					<img className="logo" src="./logo" alt="Happy Box logo" />
+					<div className="main-content">
+						<div className="left">
+							<div className="hero-text">
+								<h1>Unbox Happiness!</h1>
+								<p>
+									Sorprende a esa persona espceial con un regalo fuera de lo
+									común
+								</p>
 							</div>
-							<div className="right">
-								<img
-									className="hero-image"
-									src={cajas}
-									alt="Ejemplo de cajas"
-								/>
+							<div className="cta-button">
+								<p>Descubre opciones ahora!</p>
 							</div>
+						</div>
+						<div className="right">
+							<img className="hero-image" src={cajas} alt="Ejemplo de cajas" />
 						</div>
 					</div>
 				</div>
+
+				<div>Como funciona</div>
 			</div>
 		);
 	}
