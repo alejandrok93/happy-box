f2c469feaaff723a61372166223f805a29a9215d
alejandrok93 <alejandrok93@gmail.com>
2019-04-20 09:51:52 -0700

    Added second part in new file

---
+++ happy-box/src/components/ComoFunciona.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const ComoFunciona = props => {
+	return (
+		<div className="como-funciona-container">
+			<div className="step">step 1</div>
+			<div className="step">step 2</div>
+			<div className="step">step 3</div>
+		</div>
+	);
+};
